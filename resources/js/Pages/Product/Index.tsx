da212df092e96180a247385b09f30c05de1ddbc0
Rupert Señga <pertsenga@gmail.com>
2024-11-04 03:43:49 +0800

    Show Product index component as root in routes

---
+++ resources/js/Pages/Product/Index.tsx
@@ -0,0 +1,10 @@
+import GuestLayout from '@/Layouts/GuestLayout';
+
+export default function Index() {
+
+    return (
+        <GuestLayout>
+          <h1>Hello World</h1>
+        </GuestLayout>
+    );
+}
