099deeebe7ea3a3e8a7b47341b8fce22e85dfb6b
Rupert Señga <pertsenga@gmail.com>
2024-11-04 04:12:56 +0800

    Setup ProductController actions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
     public function index()
     {
-        //
+        $products = Http::get('https://dummyjson.com/products');
+
+        return response($products);
+    }
+
+    public function search(Request $request)
+    {
+        $searchTerm = $request->input('q') ?? '';
+
+        $products = Http::get("https://dummyjson.com/products/search?q=$searchTerm");
+
+        return response($products);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Product/Index');
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/search', [ProductController::class, 'search']);
 
 require __DIR__.'/auth.php';
